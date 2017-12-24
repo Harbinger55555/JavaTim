7546085f484f276fb70c509ebedc3a2cf661947e
Kalesh-Singh <kaleshsingh96@gmail.com>
2017-12-24 12:11:36 -0500

    Keywords and Expressions

---
+++ 05/Keywords.java
@@ -0,0 +1,13 @@
+public class Keywords {
+
+    public static void main (String[] args) {
+        
+        // 1 mile = 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+      
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
