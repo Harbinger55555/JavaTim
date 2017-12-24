8a5381d691461155bdcd12ab615d9df66d9b7cf7
Kalesh-Singh <kaleshsingh96@gmail.com>
2017-12-24 12:12:58 -0500

    Statements Whitespace and Indentation

---
+++ 05/WhiteSpace.java
@@ -0,0 +1,22 @@
+public class WhiteSpace {
+
+    public static void main (String[] args) {
+
+        // Statements
+   
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is " + 
+                           "another test." + 
+                           " Still more!");
+
+        System.out.println("This is another one");
+
+        if (myVariable == 50)
+            System.out.println("Printed!");
+
+    }
+
+}
