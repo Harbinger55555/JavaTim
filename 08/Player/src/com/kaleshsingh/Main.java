e6925f224c3b31bc2d40aba229cbc05601f27485
Kalesh-Singh <kaleshsingh96@gmail.com>
2017-12-27 23:27:46 -0500

    Good coding practice - Encapsulation

---
+++ 08/Player/src/com/kaleshsingh/Main.java
@@ -0,0 +1,11 @@
+package com.kaleshsingh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EnhancedPlayer player = new EnhancedPlayer("Kalesh", 200, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+
+    }
+
+}
